a3f38dfc079050c8c95f51c37534a0312161e404
selena1105 <1428302949@qq.com>
2022-10-23 10:05:20 +0800

    first

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from http.cookies import BaseCookie
 import PyQt5
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets,uic
+from PyQt5.QtWidgets import QApplication
 
-print("hello")
-print("hi")
+class Book:
+    def __init__(self):
+        # 从文件中加载UI定义
+        self.ui = uic.loadUi("ui.ui")
+
+
+if __name__ == "__main__":
+    App = QApplication(sys.argv)    # 创建QApplication对象，作为GUI主程序入口
+    book = Book()
+    book.ui.show()               # 显示主窗体
+    sys.exit(App.exec_())   # 循环中等待退出程序
